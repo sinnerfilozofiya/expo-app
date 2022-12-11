b86db847fb0f98105a46e2d81537bea753ea499e
emir <alptekine2@gmail.com>
2022-12-12 00:57:44 +0300

    first commit

---
+++ screens/Login.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+
+import { useWalletConnect } from '@walletconnect/react-native-dapp';
+
+const shortenAddress = (address: string) => {
+  return `${address.slice(0, 6)}...${address.slice(
+    address.length - 4,
+    address.length
+  )}`;
+}
+
+export default function Login({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const connector = useWalletConnect();
+
+  const connectWallet = React.useCallback(() => {
+    return connector.connect();
+  }, [connector]);
+
+  const killSession = React.useCallback(() => {
+    return connector.killSession();
+  }, [connector]);
+  const loginPressed = () => {
+    navigation.navigate('Bottom')
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>wallet</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      {!connector.connected && (
+        <TouchableOpacity onPress={connectWallet} style={styles.buttonStyle}>
+          <Text style={styles.buttonTextStyle}>Connect a Wallet</Text>
+        </TouchableOpacity>
+      )}
+      {!!connector.connected && (
+        <>
+        <Text>{shortenAddress(connector.accounts[0])}</Text>
+        <TouchableOpacity onPress={loginPressed} style={styles.buttonStyle}>
+          <Text style={styles.buttonTextStyle}>welcome</Text>
+        </TouchableOpacity>
+      </>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  buttonStyle: {
+    backgroundColor: "#3399FF",
+    borderWidth: 0,
+    color: "#FFFFFF",
+    borderColor: "#3399FF",
+    height: 40,
+    alignItems: "center",
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  buttonTextStyle: {
+    color: "#FFFFFF",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
